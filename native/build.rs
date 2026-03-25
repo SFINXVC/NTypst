c9dd0c0370793ec91916875a60969ca2443764e6
SFINXV <145094765+SFINXVC@users.noreply.github.com>
2026-03-26 02:47:15 +0700

    feat: enhance build process to collect and upload generated bindings

--- native/build.rs
+++ native/build.rs
@@ -1,8 +1,13 @@
+use std::path::PathBuf;
+
 fn main() {
+    let out_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    let out_file = out_dir.join("NativeMethods.g.cs");
+
     csbindgen::Builder::default()
         .input_extern_file("src/lib.rs")
         .csharp_dll_name("typst_native")
         .csharp_namespace("NTypst.Native")
-        .generate_csharp_file("../src/NTypst.Native/Generated/NativeMethods.g.cs")
-        .unwrap()
+        .generate_csharp_file(out_file)
+        .expect("failed to generate bindings");
 }
